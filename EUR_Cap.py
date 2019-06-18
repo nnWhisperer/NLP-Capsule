47e38a928471baa9674344dfa5b057f810693462
bbulut <13225349+nnWhisperer@users.noreply.github.com>
2019-06-18 12:51:09 +0300

    Moved the dataset class to its own file.

--- EUR_Cap.py
+++ EUR_Cap.py
@@ -6,45 +6,18 @@ import os
 import json
 import time
 from torch.optim import Adam
-import torch.utils.data
 from network import CapsNet_Text, BCE_loss
 from w2v import load_word2vec
-import data_helpers
 import sys
 from utils import set_seeds, get_available_devices
 from tqdm import tqdm
 from tensorboardX import SummaryWriter
 import torch.nn.functional as F
+from dataset_classes import simpleDataset
 """
 TODO: is it OK to set seeds by calling a method in another file?
 TODO: is it necessary to call torch.cuda.empty_cache() that often?
 """
-
-
-class simpleDataset(torch.utils.data.Dataset):
-
-    def __init__(self, args):
-        super(simpleDataset, self).__init__()
-        self.X_trn, self.Y_trn, self.Y_trn_o, self.X_tst, self.Y_tst, self.Y_tst_o, self.vocabulary, self.vocabulary_inv = data_helpers.load_data(args.dataset, max_length=args.sequence_length, vocab_size=args.vocab_size)
-        self.is_train = True
-
-    def get_class_count(self):
-        return self.Y_trn.shape[1]
-
-    def get_vocabulary_inv(self):
-        return self.vocabulary_inv
-
-    def __len__(self):
-        return self.X_trn.shape[0] if self.is_train else self.X_tst.shape[0]
-
-    def set_train(self):
-        self.is_train = True
-
-    def set_val(self):
-        self.is_train = False
-
-    def __getitem__(self, idx):
-        return (self.X_trn[idx], self.Y_trn_o[idx]) if self.is_train else (self.X_tst[idx], self.Y_tst[idx])
 
 
 def my_collate_fn(examples):
